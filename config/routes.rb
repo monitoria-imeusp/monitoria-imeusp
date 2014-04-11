a38dc2851a78e71c697e25ad144833fa2f084b6c
Lucas Takeshi <lucastakeshipalma@gmail.com>
2014-04-11 15:36:08 -0300

    [Login Professor] Criando login do professor usando o devise Signed off by: Lucas G Gassert <lggassert@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'home/index'
 
   devise_for :admins
+  devise_for :professors
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140409175436) do
+
+ActiveRecord::Schema.define(version: 20140411173912) do
 
   create_table "admins", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -37,6 +38,15 @@ ActiveRecord::Schema.define(version: 20140409175436) do
     t.string   "nusp"
     t.string   "department"
     t.string   "email"
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "super_professor", default: false
