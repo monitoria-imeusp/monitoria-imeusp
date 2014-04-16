2e388e0c981614fddc3d631c20a93ac9b2bc35bc
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2014-04-16 15:55:37 -0300

    [pedido-monitoria] Validando que nenhum campo esteja vazio.
    Signed off by: Lucas Gassert < lggassert@gmail.com >
    Signed off by: Jackson Souze < jacksonjsouza@gmail.com >

--- app/models/request_for_teaching_assistant.rb
+++ app/models/request_for_teaching_assistant.rb
@@ -1,4 +1,10 @@
 class RequestForTeachingAssistant < ActiveRecord::Base
   include ActiveModel::Validations
-  validates :priority, inclusion: { in: 0..2 }
+  validates :professor_id, presence: true
+  validates :subject, presence: true
+  validates :requestedNumber, presence: true, inclusion: { in: 1..32 }
+  validates :priority, presence: true, inclusion: { in: 0..2 }
+  validates :student_assistance, presence: true
+  validates :work_correction, presence: true
+  validates :test_oversight, presence: true
 end
