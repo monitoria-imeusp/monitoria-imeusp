abf9ca8259ddc84daa3775389faadc95c7a79606
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2014-04-16 15:43:27 -0300

    [pedido-monitoria] Validando prioridade do pedido.
    Signed off by: Jackson Souza < jacksonjsouza@gmail.com >
    Signed off by: Lucas Gassert < lggassert@gmail.com >

--- app/helpers/request_for_teaching_assistants_helper.rb
+++ app/helpers/request_for_teaching_assistants_helper.rb
@@ -1,2 +1,9 @@
 module RequestForTeachingAssistantsHelper
+  def priorityOptions
+    [
+      ['Imprescindível', 0],
+      ['Extremamente necessário, mas não imprescindível', 1],
+      ['Importante, porém posso abrir mão do auxílio de um monitor', 2]
+    ]
+  end
 end
--- app/models/request_for_teaching_assistant.rb
+++ app/models/request_for_teaching_assistant.rb
@@ -1,2 +1,4 @@
 class RequestForTeachingAssistant < ActiveRecord::Base
+  include ActiveModel::Validations
+  validates :priority, inclusion: { in: 0..2 }
 end
