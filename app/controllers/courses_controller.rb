959d655929bbed24ca61767d25092adaa0fafffc
Heitor Reis Ribeiro <marcheing@gmail.com>
2014-04-23 15:22:42 -0300

    [Crud course] Cadastrando a disciplina como superprofessor
    Signed off by: Lucas Takeshi <lucastakeshipalma@gmail.com>
    Signed off by: Andre Pestana <andre.pest92@gmail.com>

---
+++ app/controllers/courses_controller.rb
@@ -0,0 +1,33 @@
+class CoursesController < ApplicationController
+    before_action :authenticate_professor!, :except => [:show, :index]
+    before_action do
+        redirect_to :new_professor_session_path unless current_professor.super_professor?
+    end
+
+    def new
+        @course = Course.new
+    end
+
+    def create
+        @course = Course.new(course_params)
+        if @course.save
+            redirect_to @course
+        else
+            render 'new'
+        end
+    end
+
+    def show
+        if Course.exists?(params[:id])
+            @course = Course.find(params[:id])
+        else
+            redirect_to courses_path
+        end
+    end
+
+
+    private
+        def course_params
+            params.require(:course).permit(:name, :course_code)
+        end
+end
