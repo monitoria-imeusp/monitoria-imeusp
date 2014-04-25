24caa07d3b716140b17aa5dffa9190cfedc07253
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2014-04-25 18:00:07 -0300

    [pedido-monitoria] Testes para o controller de pedidos.
    Signed off by: Heitor Reis <marcheing@gmail.com>

--- spec/controllers/request_for_teaching_assistants_controller_spec.rb
+++ spec/controllers/request_for_teaching_assistants_controller_spec.rb
@@ -20,23 +20,46 @@ require 'spec_helper'
 
 describe RequestForTeachingAssistantsController do
 
+  include Devise::TestHelpers
+
   # This should return the minimal set of attributes required to create a valid
   # RequestForTeachingAssistant. As you add validations to RequestForTeachingAssistant, be sure to
   # adjust the attributes here as well.
   let(:valid_attributes) { {
-      "professor_id" => "1",
-      "subject" => "MAC0122semespaço",
-      "requestedNumber" => 1,
-      "priority" => 0,
-      "student_assistance" => true,
-      "work_correction" => true,
-      "test_oversight" => true
-    } }
+    "professor_id" => "1",
+    "subject" => "MAC0122semespaço",
+    "requestedNumber" => 1,
+    "priority" => 0,
+    "student_assistance" => true,
+    "work_correction" => false,
+    "test_oversight" => true
+  } }
+
+  let(:not_owned_attributes) { {
+    "professor_id" => "2",
+    "subject" => "MAC0424",
+    "requestedNumber" => 2,
+    "priority" => 2,
+    "student_assistance" => true,
+    "work_correction" => false,
+    "test_oversight" => false
+  } }
+
+  let(:valid_professor) { {
+    "id" => 1,
+    "password" => "prof-123",
+    "email" => "prof@ime.usp.br"
+  } }
 
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
   # RequestForTeachingAssistantsController. Be sure to keep this updated too.
-  let(:valid_session) { {} }
+  let(:valid_session) { { } }
+
+  before :each do
+    professor = Professor.create! valid_professor
+    sign_in :professor, professor
+  end
 
   describe "GET index" do
     it "assigns all request_for_teaching_assistants as @request_for_teaching_assistants" do
@@ -62,19 +85,37 @@ describe RequestForTeachingAssistantsController do
   end
 
   describe "GET edit" do
-    it "assigns the requested request_for_teaching_assistant as @request_for_teaching_assistant" do
-      request_for_teaching_assistant = RequestForTeachingAssistant.create! valid_attributes
-      get :edit, {:id => request_for_teaching_assistant.to_param}, valid_session
-      assigns(:request_for_teaching_assistant).should eq(request_for_teaching_assistant)
+    describe "a request from the signed professor" do
+      it "assigns the requested request_for_teaching_assistant as @request_for_teaching_assistant" do
+        request_for_teaching_assistant = RequestForTeachingAssistant.create! valid_attributes
+        get :edit, {:id => request_for_teaching_assistant.to_param}, valid_session
+        assigns(:request_for_teaching_assistant).should eq(request_for_teaching_assistant)
+      end
+    end
+
+    describe "edit a request not from the signed professor" do
+      it "redirects back to the request list" do
+        request_for_teaching_assistant = RequestForTeachingAssistant.create! not_owned_attributes
+        get :edit, {:id => request_for_teaching_assistant.to_param}, valid_session
+        response.should redirect_to request_for_teaching_assistants_path
+      end
     end
   end
 
   describe "POST create" do
+
+    it "uses the current signed professor's id" do
+      post :create, {:request_for_teaching_assistant => valid_attributes}, valid_session
+      assigns(:request_for_teaching_assistant).professor_id.should be(valid_professor["id"])
+    end
+
     describe "with valid params" do
       it "creates a new RequestForTeachingAssistant" do
-        expect {
-          post :create, {:request_for_teaching_assistant => valid_attributes}, valid_session
-        }.to change(RequestForTeachingAssistant, :count).by(1)
+        count_before = RequestForTeachingAssistant.count
+        assert(!RequestForTeachingAssistant.exists?(valid_attributes[:id]))
+        post :create, {:request_for_teaching_assistant => valid_attributes}, valid_session
+        count_after = RequestForTeachingAssistant.count
+        (count_before+1).should equal(count_after)
       end
 
       it "assigns a newly created request_for_teaching_assistant as @request_for_teaching_assistant" do
@@ -93,7 +134,7 @@ describe RequestForTeachingAssistantsController do
       it "assigns a newly created but unsaved request_for_teaching_assistant as @request_for_teaching_assistant" do
         # Trigger the behavior that occurs when invalid params are submitted
         RequestForTeachingAssistant.any_instance.stub(:save).and_return(false)
-        post :create, {:request_for_teaching_assistant => { "professor_id" => "invalid value" }}, valid_session
+        post :create, {:request_for_teaching_assistant => {}}, valid_session
         assigns(:request_for_teaching_assistant).should be_a_new(RequestForTeachingAssistant)
       end
 
@@ -101,7 +142,7 @@ describe RequestForTeachingAssistantsController do
         # Trigger the behavior that occurs when invalid params are submitted
         RequestForTeachingAssistant.any_instance.stub(:save).and_return(false)
         post :create, {:request_for_teaching_assistant => { "professor_id" => "invalid value" }}, valid_session
-        response.should render_template("new")
+        response.should render_template :new
       end
     end
   end
@@ -151,17 +192,28 @@ describe RequestForTeachingAssistantsController do
   end
 
   describe "DELETE destroy" do
-    it "destroys the requested request_for_teaching_assistant" do
-      request_for_teaching_assistant = RequestForTeachingAssistant.create! valid_attributes
-      expect {
+
+    describe "a request from the signed professor" do
+      it "destroys the requested request_for_teaching_assistant" do
+        request_for_teaching_assistant = RequestForTeachingAssistant.create! valid_attributes
+        expect {
+          delete :destroy, {:id => request_for_teaching_assistant.to_param}, valid_session
+        }.to change(RequestForTeachingAssistant, :count).by(-1)
+      end
+
+      it "redirects to the request_for_teaching_assistants list" do
+        request_for_teaching_assistant = RequestForTeachingAssistant.create! valid_attributes
         delete :destroy, {:id => request_for_teaching_assistant.to_param}, valid_session
-      }.to change(RequestForTeachingAssistant, :count).by(-1)
+        response.should redirect_to(request_for_teaching_assistants_url)
+      end
     end
 
-    it "redirects to the request_for_teaching_assistants list" do
-      request_for_teaching_assistant = RequestForTeachingAssistant.create! valid_attributes
-      delete :destroy, {:id => request_for_teaching_assistant.to_param}, valid_session
-      response.should redirect_to(request_for_teaching_assistants_url)
+    describe "a request not from the signed professor" do
+      it "redirects back to the request list" do
+        request_for_teaching_assistant = RequestForTeachingAssistant.create! not_owned_attributes
+        delete :destroy, {:id => request_for_teaching_assistant.to_param}, valid_session
+        response.should redirect_to request_for_teaching_assistants_path
+      end
     end
   end
 
