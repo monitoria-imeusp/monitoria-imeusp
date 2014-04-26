790057389af70ee5887669162498fb7057a323d5
Leonardo Schäffer <leonardo.schaffer@gmail.com>
2014-04-26 13:12:51 -0300

    [pedido-monitoria-testes] Consertados os testes do controller do professor, agora eles fazem login
    Signed off by: Jackson Souza <jacksonjsouza@gmail.com>

--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -2,16 +2,14 @@ module ControllerMacros
   def login_admin
     before(:each) do
       @request.env["devise.mapping"] = Devise.mappings[:admin]
-      sign_in FactoryGirl.create(:admin) # Using factory girl as an example
+      sign_in FactoryGirl.create(:admin)
     end
   end
 
   def login_professor
     before(:each) do
       @request.env["devise.mapping"] = Devise.mappings[:professor]
-      professor = FactoryGirl.create(:professor)
-      #professor.confirm! # or set a confirmed_at inside the factory. Only necessary if you are using the "confirmable" module
-      sign_in professor
+      sign_in FactoryGirl.create(:professor)
     end
   end
 end
