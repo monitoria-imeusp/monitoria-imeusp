0211c5da1b7caea3565d18fb6b1ad263a4843d34
Andre Pestana <andre.pest92@gmail.com>
2014-04-30 15:02:21 -0300

    [Crud course] Iniciado testes de controller
    Signed off by: Wilson Kazuo Mizutani <kazuo256@gmail.com>

--- spec/controllers/courses_controller_spec.rb
+++ spec/controllers/courses_controller_spec.rb
@@ -2,4 +2,24 @@ require 'spec_helper'
 
 describe CoursesController do
 
+	 include Devise::TestHelpers
+
+	let(:valid_admin){{
+		"email" => "kazuo@ime.usp.br",
+		"password" => "admin123"
+		}}
+	before do
+		@course = mock_model(Course)
+		admin = Admin.create! valid_admin
+    	sign_in :admin, admin
+	end
+
+	describe 'new' do
+		it {
+			Course.should_receive(:new).and_return(@course)
+			get :new
+			assigns(:course).should eq(@course)
+		}
+	end
+
 end
