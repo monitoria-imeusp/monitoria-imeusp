0b1cabe771bc972e311d049ff942d8455fcdf0e3
Andre Pestana <andre.pest92@gmail.com>
2014-05-07 15:37:12 -0300

    [Crud course] Mais testes de controle adicionados Signed off by: Wilson Kazuo Mizutani <kazuo256@gmail.com>

--- spec/controllers/courses_controller_spec.rb
+++ spec/controllers/courses_controller_spec.rb
@@ -8,8 +8,11 @@ describe CoursesController do
 		"email" => "kazuo@ime.usp.br",
 		"password" => "admin123"
 		}}
+
+
 	before do
 		@course = mock_model(Course)
+		@id = '50'
 		admin = Admin.create! valid_admin
     	sign_in :admin, admin
 	end
@@ -22,4 +25,45 @@ describe CoursesController do
 		}
 	end
 
+	describe 'create' do
+	  before :each do
+		@params = { course: {name: 'materia'}}
+	    Course.should_receive(:new).with(any_args).and_return(@course)
+	  end
+
+      context 'fails to save' do
+      	before :each do
+          @course.should_receive(:save).and_return(false)
+          post :create, @params
+        end
+        it { should render_template :new }
+      end
+
+      context 'succeeds to save' do
+        before :each do
+          @course.should_receive(:save).and_return(true)
+          post :create, @params
+      	end
+        it { should redirect_to @course }
+      end
+	end
+
+	describe 'show' do
+		context 'course does exist' do
+		  before :each do
+			Course.should_receive(:exists?).with(@id).and_return(true)
+        	Course.should_receive(:find).with(@id).and_return(@course)
+        	get :show, id: @id
+        	assigns(:course).should eq(@course)
+          end
+          it {should render_template :show}
+		end
+		context 'course does not exist' do
+		  before :each do
+			Course.should_receive(:exists?).with(@id).and_return(false)
+			get :show, id: @id
+		  end
+		  it {should redirect_to courses_path}
+		end
+	end
 end
