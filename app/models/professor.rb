8b4bf32e42a7f67056f7431886824f4068a1d256
Jackson J Souza <jacksonjsouza@gmail.com>
2014-05-09 16:39:56 -0300

    Added relashionship between professor and request_for_teatching_assistant classes on Professor model

--- app/models/professor.rb
+++ app/models/professor.rb
@@ -1,3 +1,4 @@
 class Professor < ActiveRecord::Base
+	has_many :request_for_teaching_assistant
   devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => [:nusp]
 end
