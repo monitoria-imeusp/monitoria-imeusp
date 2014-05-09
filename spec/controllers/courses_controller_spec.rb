659acf51fe19782cf70b0f6f364bad7b1b559fa2
Andre Pestana <andre.pest92@gmail.com>
2014-05-09 14:28:57 -0300

    [Crud course] Teste de controller finalizados

--- spec/controllers/courses_controller_spec.rb
+++ spec/controllers/courses_controller_spec.rb
@@ -66,4 +66,91 @@ describe CoursesController do
 		  it {should redirect_to courses_path}
 		end
 	end
+
+
+  describe 'index' do
+    before :each do
+      Course.should_receive(:all)
+      get :index
+    end
+    it { should render_template :index }
+  end
+
+  describe 'edit' do
+    context 'course does exist' do
+      before :each do
+        Course.should_receive(:exists?).with(@id).and_return(true)
+        Course.should_receive(:find).with(@id).and_return(@course)
+        get :edit, id: @id
+        assigns(:course).should eq(@course)
+      end
+      it { should render_template :edit }
+    end
+
+    context "course does not exist"  do
+      before :each do
+        Course.should_receive(:exists?).with(@id).and_return(false)
+        get :edit, id: @id
+      end
+      it { should redirect_to courses_path }
+    end
+  end
+
+  describe 'update' do
+    context 'a course that does not exist' do
+      before :each do
+        Course.should_receive(:exists?).with(@id).and_return(false)
+        put :update, id: @id, course: { id: @id }
+      end
+      it { should redirect_to course_path }
+    end
+
+    context 'a course that does exist' do
+      before do
+        Course.should_receive(:exists?).with(@id).and_return(true)
+        Course.should_receive(:find).with(@id).and_return(@course)
+      end
+
+      context 'but fails to update' do
+        before :each do
+          @course.should_receive(:update).with(any_args).and_return(false)
+          put :update, id: @id, course: { id: @id }
+          assigns(:course).should eq(@course)
+        end
+        it { should render_template :edit }
+      end
+
+      context 'and succeeds to update' do
+        before :each do
+          @course.should_receive(:update).with(any_args).and_return(true)
+          put :update, id: @id, course: { id: @id }
+          assigns(:course).should eq(@course)          
+        end
+        it { should redirect_to @course }
+      end
+    end
+  end
+
+  describe "destroy" do
+
+    describe "destroy a course" do
+      before :each do
+      	Course.should_receive(:find).with(@id).and_return(@course)
+        @course.should_receive(:destroy)
+      end
+
+      pending "should we use fixtures?" do
+        it "destroys the course" do
+          expect {
+            delete :destroy, {:id => @id}
+          }.to change(Course, :count).by(-1)
+        end
+      end
+
+      it "redirects to the courses list" do
+        delete :destroy, {:id => @id}
+        response.should redirect_to(courses_url)
+      end
+    end
+  end
 end
