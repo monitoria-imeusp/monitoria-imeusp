f3eb78729cfcb459d367cb9ec051530526e98408
Lucas Takeshi Rodrigues Palma <lucastakeshipalma@gmail.com>
2014-06-27 17:19:57 -0300

    [professor_rank] Trocando o super professor pelo professor rank
    Signed off by: Jackson Souza <jacksonjsouza@gmail.com>

--- app/helpers/professors_helper.rb
+++ app/helpers/professors_helper.rb
@@ -1,2 +1,9 @@
 module ProfessorsHelper
+	def professorRankOptions
+    [
+      ['Comum', 0],
+      ['Membro da comissão de monitoria', 1],
+      ['Chefe da comissão de monitoria', 2]
+    ]
+  end
 end
