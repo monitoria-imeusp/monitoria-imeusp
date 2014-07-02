120ecf10b78c343a283c0c597ab307d3b592dc08
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2014-07-02 16:47:04 -0300

    [dev] Trying to fix production mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,6 +73,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_options = {from: 'sistemamonitoria@ime.usp.br'}
+  config.action_mailer.default_url_options = { host: 'monitoria.ime.usp.br' }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
