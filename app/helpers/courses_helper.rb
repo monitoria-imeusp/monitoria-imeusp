f8f32adfb0be83102405e6459f0c776755c8c527
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2014-07-06 14:31:31 -0300

    [offered_courses] Adicionei campo para nível de escolaridade no formulário dos cursos

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -1,2 +1,8 @@
 module CoursesHelper
+  def educational_levels
+    [
+      ['Graduação', 0],
+      ['Pós-graduação', 1]
+    ]
+  end
 end
--- app/models/course.rb
+++ app/models/course.rb
@@ -3,6 +3,7 @@ class Course < ActiveRecord::Base
   belongs_to :department
 
   validates :department_id, presence: true
+  validates :educational_level, presence: true, inclusion: { in: 0..1 }
 
   def full_name
     course_code + " - " + name
