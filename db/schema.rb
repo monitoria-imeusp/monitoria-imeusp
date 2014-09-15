cfc76506692f1788ddc117b197a59339fc763a45
Andre Pestana <andre.pest92@gmail.com>
2014-09-15 15:02:52 -0300

    Mudança do campo Nome para Nome Completo no cadastro do aluno.

--- db/schema.rb
+++ db/schema.rb
@@ -142,14 +142,14 @@ ActiveRecord::Schema.define(version: 20140710234408) do
     t.string   "tel"
     t.string   "cel"
     t.string   "email"
-    t.boolean  "has_bank_account"
+    t.boolean  "has_bank_account",       default: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
