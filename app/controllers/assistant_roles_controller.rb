90d226c6c5f5ef0eabedf579b61ce986751d5166
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2014-10-31 14:52:08 -0200

    [assistant-role] Arrumei criação de inscrição

--- app/controllers/assistant_roles_controller.rb
+++ app/controllers/assistant_roles_controller.rb
@@ -15,11 +15,15 @@ class AssistantRolesController < ApplicationController
   def create
     @assistant_role = AssistantRole.new assistant_role_params
     if @assistant_role.save
-      format.html { redirect_to @assistant_role.request_for_teaching_assistant, notice: 'Monitor eleito com sucesso.' }
-      format.json { render action: 'show', status: :created, location: @assistant_role.request }
+      respond_to do |format|
+        format.html { redirect_to @assistant_role.request_for_teaching_assistant, notice: 'Monitor eleito com sucesso.' }
+        format.json { render action: 'show', status: :created, location: @assistant_role.request }
+      end
     else
-      format.html { redirect_to @assistant_role.request }
-      format.json { render json: @assistant_role.errors, status: :unprocessable_entity }
+      respond_to do |format|
+        format.html { redirect_to @assistant_role.request }
+        format.json { render json: @assistant_role.errors, status: :unprocessable_entity }
+      end
     end
   end
 
