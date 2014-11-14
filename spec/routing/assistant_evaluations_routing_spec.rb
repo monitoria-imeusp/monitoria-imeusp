e30a520b56b2a98e56e72d160ec6c7fcd8030f00
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2014-11-14 14:59:00 -0200

    [assistant-evaluation] Tirando testes de rspec não usados

--- spec/routing/assistant_evaluations_routing_spec.rb
+++ spec/routing/assistant_evaluations_routing_spec.rb
@@ -3,16 +3,12 @@ require "spec_helper"
 describe AssistantEvaluationsController do
   describe "routing" do
 
-    it "routes to #index" do
-      get("/assistant_evaluations").should route_to("assistant_evaluations#index")
+    it "routes to #index_for_student" do
+      get("/assistant_evaluations/for_student/1").should route_to("assistant_evaluations#index_for_student", :student_id => "1")
     end
 
     it "routes to #new" do
-      get("/assistant_evaluations/new").should route_to("assistant_evaluations#new")
-    end
-
-    it "routes to #show" do
-      get("/assistant_evaluations/1").should route_to("assistant_evaluations#show", :id => "1")
+      get("/assistant_evaluations/1/new").should route_to("assistant_evaluations#new", :assistant_role_id => "1")
     end
 
     it "routes to #edit" do
@@ -25,10 +21,6 @@ describe AssistantEvaluationsController do
 
     it "routes to #update" do
       put("/assistant_evaluations/1").should route_to("assistant_evaluations#update", :id => "1")
-    end
-
-    it "routes to #destroy" do
-      delete("/assistant_evaluations/1").should route_to("assistant_evaluations#destroy", :id => "1")
     end
 
   end
