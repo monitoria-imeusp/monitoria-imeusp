7a62fa894a4dc284b674c420d93fc06c2c6b7519
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2014-11-27 17:35:33 -0200

    [evaluation-notes] Informações do aluno e instruções na ficha de avaliação

--- app/models/assistant_evaluation.rb
+++ app/models/assistant_evaluation.rb
@@ -7,4 +7,22 @@ class AssistantEvaluation < ActiveRecord::Base
   validates :overall, presence: true, inclusion: { in: 0..2 }
   validates :comment, presence: true
   belongs_to :assistant_role
+
+  def semester
+    assistant_role.semester
+  end
+
+  def professor
+    assistant_role.professor
+  end
+
+  def student
+    assistant_role.student
+  end
+
+  def course
+    assistant_role.course
+  end
+
+
 end
