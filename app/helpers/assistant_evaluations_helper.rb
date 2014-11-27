75b297f0e49285c47a5e8e791ab885b1953f06c0
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2014-11-27 17:45:41 -0200

    [evaluation-notes] Ficha de avaliação usa radio boxes now

--- app/helpers/assistant_evaluations_helper.rb
+++ app/helpers/assistant_evaluations_helper.rb
@@ -1,2 +1,18 @@
 module AssistantEvaluationsHelper
+  def evaluation_grades
+    [
+      ['Ótimo', 0],
+      ['Bom', 1],
+      ['Regular', 2]
+    ]
+  end
+
+  def criteria
+    [
+      :ease_of_contact,
+      :efficiency,
+      :reliability,
+      :overall
+    ]
+  end
 end
