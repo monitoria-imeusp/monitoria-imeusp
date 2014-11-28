45aab2507f2f6f107416961dd4e113ab99c9a6df
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2014-11-28 13:44:27 -0200

    [organize-sidebar] Separei melhor as categorias de opções
    Tentei separar em templates, mas não deu certo. O HTML continua meio
    ruim de ler, mas está menos pior.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,32 @@ module ApplicationHelper
       [department.code, department.id]
     end).unshift ["", ""]
   end
+
+  def signed_in?
+    admin_signed_in? or secretary_signed_in? or professor_signed_in? or student_signed_in?
+  end
+
+  def current_user
+    if admin_signed_in?
+      current_admin
+    elsif secretary_signed_in?
+      current_secretary
+    elsif professor_signed_in?
+      current_professor
+    elsif student_signed_in?
+      current_student
+    end
+  end
+
+  def destroy_session_path
+    if admin_signed_in?
+      destroy_admin_session_path
+    elsif secretary_signed_in?
+      destroy_secretary_session_path
+    elsif professor_signed_in?
+      destroy_professor_session_path
+    elsif student_signed_in?
+      destroy_student_session_path
+    end
+  end
 end
