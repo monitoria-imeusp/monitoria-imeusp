2c4138fbdf46b2638dc515b7ac146a7ea3f76961
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2014-12-18 15:03:36 -0200

    [prof] Impedindo criação de professores com nusp repetido

--- app/models/professor.rb
+++ app/models/professor.rb
@@ -1,12 +1,12 @@
 class Professor < ActiveRecord::Base
-	devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable, 
+	devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable,
 		:confirmable, :authentication_keys => [:nusp]
 
   has_many :request_for_teaching_assistant
   belongs_to :department
 
   validates :department_id, presence: true
-  validates :nusp, presence: true
+  validates :nusp, presence: true, uniqueness: true
 
   def normal_professor?
     return professor_rank == 0
