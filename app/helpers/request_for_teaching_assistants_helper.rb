aa08e79875c503116303164b646f54768e2c0cd6
Rewasvat <fernando_aluani@hotmail.com>
2015-05-08 16:24:30 -0300

    [elected_candidates] Refatoramos view de Pedidos de monitoria
    Refatoramos as tabelas de candidaturas para serem geradas por um metodo helper.
    Eles também pedem confirmacão para eleger um candidato que já foi eleito.
    
    Signed off by: Gabriel Ferreira Guilhoto <gabrielguilhoto@gmail.com>

--- app/helpers/request_for_teaching_assistants_helper.rb
+++ app/helpers/request_for_teaching_assistants_helper.rb
@@ -18,4 +18,41 @@ module RequestForTeachingAssistantsHelper
       [professor.name, professor.id]
     end).sort
   end
+  
+  def candidatures_table candidates, title
+    table = "<table>
+    <thead>
+      <tr>
+        <th>Nome</th>
+        <th>Nº USP</th>
+        <th>Já foi eleito?</th>
+        <th colspan=\"2\"></th>
+      </tr>
+    </thead>
+
+    <tbody> "
+    if !title.nil? and !title.empty?
+        table += "<h3>" + title + "</h3>"
+    end
+    candidates.each do |candidature|
+        if candidature.elected?
+            table += "<tr class=\"elected\">"
+        else
+            table += "<tr>"
+        end
+        table += "<td>" + candidature.student.name + "</td>"
+        table += "<td>" + candidature.student.nusp.to_s + "</td>"
+        table += "<td>" + show_yes_or_no(candidature.elected?) + "</td>"
+        table += "<td>" + link_to('Detalhes', candidature) + "</td>" 
+        if candidature.elected?
+            table += "<td>" + link_to('Eleger', create_assistant_role_path(@request_for_teaching_assistant, candidature.student), 
+            method: :post, data: {confirm: "Este estudante já foi eleito para monitor(a) de outra disciplina, está certo que deseja elegê-lo(a) novamente?"}) + "</td>"
+        else
+            table += "<td>" + link_to('Eleger', create_assistant_role_path(@request_for_teaching_assistant, candidature.student), method: :post) + "</td>"
+        end
+        table += "</tr>"
+    end
+    table += "</tbody> </table>"
+    table.html_safe
+  end
 end
