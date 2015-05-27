bb72268077d4cfe61361d95e62d490cc15833259
Rewasvat <fernando_aluani@hotmail.com>
2015-05-27 16:17:22 -0300

    [test day] Refactored and implemented pending tests for Assistant Role Controller
    Signed off by: @Tuiuiu

--- app/controllers/assistant_roles_controller.rb
+++ app/controllers/assistant_roles_controller.rb
@@ -15,15 +15,22 @@ class AssistantRolesController < ApplicationController
 
   def create
     @assistant_role = AssistantRole.new assistant_role_params
-    if @assistant_role.save
+    student_exists = Student.exists? @assistant_role.student_id
+    request_exists = RequestForTeachingAssistant.exists? @assistant_role.request_for_teaching_assistant_id
+    if student_exists and request_exists and @assistant_role.save
       respond_to do |format|
         BackupMailer.new_assistant_role(@assistant_role, current_creator).deliver
         format.html { redirect_to @assistant_role.request_for_teaching_assistant, notice: 'Monitor eleito com sucesso.' }
-        format.json { render action: 'show', status: :created, location: @assistant_role.request }
+        format.json { render action: 'show', status: :created, location: @assistant_role.request_for_teaching_assistant }
       end
+    elsif request_exists
+      respond_to do |format|
+        format.html { redirect_to @assistant_role.request_for_teaching_assistant, notice: 'Erro ao criar monitor.' }
+        format.json { render json: @assistant_role.errors, status: :unprocessable_entity }
+      end  
     else
       respond_to do |format|
-        format.html { redirect_to @assistant_role.request }
+        format.html { redirect_to '/', notice: 'Erro ao criar monitor.' }
         format.json { render json: @assistant_role.errors, status: :unprocessable_entity }
       end
     end
