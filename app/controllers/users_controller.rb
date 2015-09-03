031bbe5208f12fb461b05430fdb0b0250560e8cb
Thiago Nunes <gorobaum@gmail.com>
2015-09-03 11:15:20 -0300

    [issue_98] Usuários não podem mais se editar
    Signed off by:@Kazuo256

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,34 +10,10 @@ class UsersController < ApplicationController
     end
   end
 
-  def edit
-    if User.exists?(params[:id])
-      @user = User.find(params[:id])
-      authorization_user
-    else
-      raise ActionController::RoutingError.new('Not Found')
-    end
-  end
-
   def index
     @users = User.all
   end
-
-  def update
-    @user = User.find(params[:id])
-    authorization_user
-    if params[:user][:password].blank? && params[:user][:password_confirmation].blank?
-      params[:user].delete(:password)
-      params[:user].delete(:password_confirmation)
-    end
-    if @user.update(user_params)
-      sign_in  @user, :bypass => true
-      redirect_to @user
-    else
-      render 'edit'
-    end
-  end
-
+  
   def destroy
     @user = User.find(params[:id])
     authorization_user_destroy
