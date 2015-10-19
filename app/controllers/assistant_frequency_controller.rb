ee7e5d2e12fc1f197971a2d902e5128ee78d3cf6
Kazuo256 <kazuo256@gmail.com>
2015-10-19 13:00:04 -0200

    [frequency_manual_enabling] Página de controle de frequência
    Coloca links (como abas e dropdrown) para escolher semestre e mês

--- app/controllers/assistant_frequency_controller.rb
+++ app/controllers/assistant_frequency_controller.rb
@@ -18,24 +18,20 @@ class AssistantFrequencyController < ApplicationController
     month = params[:month]
     role = params[:role]
     id = params[:pid]
-    red_path = '/assistant_roles/for_professor/'+id
-    if params[:redirect_to_index] == "not_for_professor"
-      red_path = '/assistant_frequency'
-    end
     @assistant_frequency = AssistantFrequency.new(month: month, presence: presence, assistant_role_id: role, payment: false)
     if @assistant_frequency.save
       if presence
         respond_to do |format|
-          format.html { redirect_to red_path, notice: 'Presença marcada com sucesso.' }      
+          format.html { redirect_to assistant_roles_for_professor_path(id), notice: 'Presença marcada com sucesso.' }      
         end
       else 
         respond_to do |format|
-          format.html { redirect_to red_path, notice: 'Ausência marcada com sucesso.' }      
+          format.html { redirect_to assistant_roles_for_professor_path(id), notice: 'Ausência marcada com sucesso.' }      
         end
       end
     else
         respond_to do |format|
-          format.html { redirect_to red_path, notice: 'Erro ao marcar frequência.' }      
+          format.html { redirect_to assistant_roles_for_professor_path(id), notice: 'Erro ao marcar frequência.' }      
         end      
     end
   end
