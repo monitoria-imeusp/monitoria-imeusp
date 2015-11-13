87174a0ee526fbcebbfa16e1f46f82fa67b17bc4
Kazuo256 <kazuo256@gmail.com>
2015-11-13 10:47:40 -0200

    [dev] Fix frequency period control
    When opening or closing the frequency period, it was not possible to
    know what filter was last used, causing an error. Now it is.

--- config/routes.rb
+++ config/routes.rb
@@ -66,8 +66,8 @@ Rails.application.routes.draw do
 
   get 'assistant_frequency/monthly_control/:semester_id/:month/:filter' => 'assistant_frequency#monthly_control', as: :assistant_frequency_monthly_control
   get 'assistant_frequency/monthly_control/:semester_id/:month/:department_id/:filter' => 'assistant_frequency#monthly_control', as: :assistant_frequency_monthly_control_for_department
-  post 'assistant_frequency/open_period/:semester_id/:month' => 'assistant_frequency#open_frequency_period', as: :open_frequency_period
-  post 'assistant_frequency/close_period/:semester_id/:month' => 'assistant_frequency#close_frequency_period', as: :close_frequency_period
+  post 'assistant_frequency/open_period/:semester_id/:month/:last_filter' => 'assistant_frequency#open_frequency_period', as: :open_frequency_period
+  post 'assistant_frequency/close_period/:semester_id/:month/:last_filter' => 'assistant_frequency#close_frequency_period', as: :close_frequency_period
   post 'assistant_frequency/request_frequency/' => 'assistant_frequency#request_frequency', as: :request_frequency
   post 'assistant_frequency/mark_generic_assistant_role_frequency/' => 'assistant_frequency#mark_generic_assistant_role_frequency', as: :mark_generic_assistant_role_frequency
   post 'assistant_frequency/mark_assistant_role_frequency/' => 'assistant_frequency#mark_assistant_role_frequency', as: :mark_assistant_role_frequency
