153327641f46368d1707c0cf5c4e9839a6a86109
Kazuo256 <kazuo256@gmail.com>
2016-01-12 10:19:51 -0200

    Acesso unificado
    Adiciona acesso para alunos de pós-doc.

--- lib/omniauth/strategies/usp.rb
+++ lib/omniauth/strategies/usp.rb
@@ -58,7 +58,7 @@ module OmniAuth
       def userlink
         raw_info['vinculo'].each do |link|
           type = link['tipoVinculo']
-          if type == "ALUNOGR" or type == "ALUNOPOS" or type == "ALUNOESPGR"
+          if type == "ALUNOGR" or type == "ALUNOPOS" or type == "ALUNOESPGR" or type == "ALUNOPD"
             return :student
           elsif type == "SERVIDOR" 
             return :teacher
